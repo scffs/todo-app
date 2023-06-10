5f92d42ab1a45602220008e1b6606a9703529360
scffs <khidey@inbox.ru>
2023-06-10 13:42:20 +0700

    update eslint config & upgrade packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,49 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  env: {
+    browser: true,
+    es2021: true,
   },
-}
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint'],
+  extends: ['standard', 'airbnb', 'airbnb-typescript'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  settings: {
+    react: {
+      version: '^18.2.0',
+    },
+  },
+  rules: {
+    quotes: ['error', 'single'],
+    semi: [1, 'always'],
+    indent: 'off',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/*.{ts,tsx,js,jsx}'] }],
+    'max-len': ['warn', { code: 120 }],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+    },
+    {
+      files: ['*.tsx'],
+      rules: {
+        quotes: ['error', 'single'],
+        'react/function-component-definition': [2, {
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
+        },
+        ],
+      },
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    },
+  ],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+
+// eslint-disable-next-line import/no-unused-modules
 export default defineConfig({
   plugins: [react()],
-})
+});
